0eb6ef3a8589ffb993485d0dfb07adf4501769bc
khalil <642517@gmail.com>
2026-04-02 22:14:14 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'sudoku-v1';
+const FILES = [
+  '/sudoku-app/',
+  '/sudoku-app/index.html',
+  '/sudoku-app/manifest.json',
+  '/sudoku-app/icon-192.png',
+  '/sudoku-app/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
